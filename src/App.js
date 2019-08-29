e2b0ac98e2f04a5cdf58a985a83a37bad6e6a72b
Nayhay99 <b117009@iiit-bh.ac.in>
2019-08-29 09:51:56 +0530

    redirected signin page and included useractiontype file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Switch , Route} from 'react-router-dom'
+import {Switch , Route,Redirect} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shoppage.component'
 import Header from './components/header/header-component'
-import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up-component'
 import { auth , createUserProfileDocument } from '../src/firebase/firebase.utils'
 import {connect} from 'react-redux'
 import {setCurrentUser} from './redux/user/user.actions'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up-component';
 
 class App extends React.Component {
  
@@ -46,17 +46,28 @@ class App extends React.Component {
       <Switch>
         <Route exact path="/" component = {HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
-        <Route path="/signin" component={SignInAndSignOutPage}></Route>
+        <Route exact path="/signin" render = {
+          () => 
+            this.props.currentUser ? (
+              <Redirect to = '/'></Redirect>
+              ) : (
+                <SignInAndSignUpPage></SignInAndSignUpPage>
+              )          
+        }></Route>
       </Switch>
     </div>
   );
 }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser : user.currentUser
+} )
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser : user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
 
 //ref and snapshot objects theory
